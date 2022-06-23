043a216b6c203a6b631a8db8ecb96c458e4c57f7
unknown <coreycodes@outlook.com>
2022-06-23 03:05:28 +0100

    update

--- resources/js/store/baseUrl.js
+++ resources/js/store/baseUrl.js
@@ -1,6 +1,6 @@
 let url;
 
 if (process.env.APP_ENV === "local") url = "http://192.168.1.113:8000";
-if (process.env.APP_ENV === "production") url = "https://correct-io.herokuapp.com/";
+if (process.env.APP_ENV === "production") url = "https://correct-io.herokuapp.com";
 
 export const BASE_URL = url;
